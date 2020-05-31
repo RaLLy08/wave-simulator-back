3cfd6103ce6641b8bf9901c80ac8f3f31e779a75
RaLLy08 <uytemataaero@mail.ru>
2020-05-31 19:16:17 +0400

    some changes in front

---
+++ src/MongoDb.js
@@ -0,0 +1,14 @@
+// var MongoClient = require('mongodb').MongoClient;
+// const url = 'mongodb+srv://admin:741852963@uni-bot-gwqo8.mongodb.net/test?retryWrites=true&w=majority'
+// //const url = "mongodb+srv://admin:741852963@uni-bot-gwqo8.mongodb.net";
+// const dbOpt = { useNewUrlParser: true, useUnifiedTopology: true };
+// const dbName = 'test';
+// let _db;
+
+// MongoClient.connect(url, dbOpt, (err, client) => {
+//     if (err) {
+//         return console.log(err);
+//     } 
+//     console.log('Connection successfully');
+//     //_db = client.db(dbName);
+// });
